887a7eb00ac1c4fb82e167a2d8030a6e61cad1a4
jitunayak <jitunayak715@gmail.com>
2022-03-13 02:56:25 +0530

    homebrew essential apps

--- homebrew.sh
+++ homebrew.sh
@@ -1,3 +1,8 @@
+brew install --cask iterm2
+brew install --cask visual-studio-code
+brew install git
+
+
 # When you hover a window it will be raised to the front 
 brew tap dimentium/autoraise
 brew install autoraise
